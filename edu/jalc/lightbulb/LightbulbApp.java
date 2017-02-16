1e97688b9364b50708020809736e158483de0be5
Hodge-zf <xx.skcs@gmail.com>
2017-02-16 08:05:56 -0600

    files tracked

---
+++ edu/jalc/lightbulb/LightbulbApp.java
@@ -0,0 +1,7 @@
+
+public class LightbulbApp{
+  public static void main(String[] args) throws Exception{
+    Lightbulb lightBulb = new Lightbulb(120);
+    System.out.println("Bulb Wattage: "+lightBulb.getWattage()+'W');
+  }
+}
