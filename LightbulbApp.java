feadfb875295f1cc4d71d36228e274c951e6e947
Hodge-zf <xx.skcs@gmail.com>
2017-02-16 08:02:51 -0600

    directory cleaned up

--- LightbulbApp.java
+++
@@ -1,7 +0,0 @@
-
-public class LightbulbApp{
-  public static void main(String[] args) throws Exception{
-    Lightbulb lightBulb = new Lightbulb(120);
-    System.out.println("Bulb Wattage: "+lightBulb.getWattage()+'W');
-  }
-}
