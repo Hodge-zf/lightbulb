a4f41b31d8e70c9aab0351529f7b4adfc7cc7177
E231 <E231@volnet.jalcdom.jal.cc.il.us>
2017-04-12 08:17:24 -0500

    Prepares driver to be written

---
+++ edu/jalc/LightbulbDriver.java
@@ -0,0 +1,16 @@
+package edu.jalc;
+
+import edu.jalc.assembly.Lightbulb;
+import edu.jalc.builder.LightbulbBuilder;
+import edu.jalc.electricity.Electricity;
+import edu.jalc.contact.ContactHousing;
+import edu.jalc.contact.ContactIn;
+import edu.jalc.contact.ContactOut;
+import edu.jalc.filament.Filament;
+import edu.jalc.gas.Gas;
+import edu.jalc.glass.BulbMaterial;
+import edu.jalc.glass.BulbType;
+
+public class LightbulbDriver{
+  public static void main(String[] args) throws Exception{
+}
